3cbbe1f29852e604f83f8f9938918c8302d605a1
DESKTOP-TVFEE04\Admin <nguyenquocviet1306@gmail.com>
2017-01-10 14:05:38 +0700

    Add FoodList in InfoUniFragment

--- app/src/main/java/com/example/admin/appbus1/fragment/InfoUniFragment.java
+++ app/src/main/java/com/example/admin/appbus1/fragment/InfoUniFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -22,6 +23,7 @@ import com.example.admin.appbus1.adapters.BusForUniAdapter;
 import com.example.admin.appbus1.adapters.FoodForUniAdapter;
 import com.example.admin.appbus1.managers.Constant;
 import com.example.admin.appbus1.managers.RealmHandler;
+import com.example.admin.appbus1.managers.Utils;
 import com.example.admin.appbus1.managers.event.EventDataReady;
 import com.example.admin.appbus1.managers.event.EventUniversity;
 import com.example.admin.appbus1.models.Food;
@@ -58,6 +60,9 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
     ImageView iv_university;
     @BindView(R.id.rv_bus_for_uni)
     RecyclerView rv_bus_for_uni;
+    @BindView(R.id.progressfood)
+    ProgressBar progressBar;
+
 //    @BindView(R.id.lv_bus)
 //    ListView lv_bus;
 //    @BindView(R.id.btn_food)
@@ -73,11 +78,9 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
     private FoodRealmObject food;
     public University university;
     String IDsave;
-    
+    @BindView(R.id.rv_uni_food1)
+    RecyclerView rv_uni_food1;
 
-
-    @BindView(R.id.rv_uni_food)
-    RecyclerView rv_uni_food;
 
     public InfoUniFragment() {
         // Required empty public constructor
@@ -89,8 +92,15 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
         busAdapter = new BusForUniAdapter(university.getBus());
         rv_bus_for_uni.setAdapter(busAdapter);
         busAdapter.setOnItemClickListener(this);
+
+
 //        rv_listbus.setAdapter(busAdapter);
         busAdapter.notifyDataSetChanged();
+
+        foodForUniAdapter = new FoodForUniAdapter();
+        foodForUniAdapter.setOnItemClickListener(this);
+        rv_uni_food1.setAdapter(foodForUniAdapter);
+        foodForUniAdapter.notifyDataSetChanged();
 
     }
 
@@ -109,7 +119,7 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
         realmHandler = RealmHandler.getInstance();
         EventBus.getDefault().register(this);
         setHasOptionsMenu(true);
-        setupUI();
+        setupUI(view);
         addListener();
         return view;
     }
@@ -150,23 +160,84 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
 //            }
 //        });
     }
+    private void loadData() {
+
+        RealmHandler.getInstance().clearFoodInRealm();
+        if(!Constant.isLoadedFood){
+            progressBar.setVisibility(View.VISIBLE);
+
+            //List<University> universityList = RealmHandler.getInstance().getUniversityFromRealm();
+            //Log.d(TAG, String.valueOf(universityList));
+//            String ID = universityList.equals()
+            //Log.d(TAG,IDsave);
+
+            //String id = EventBus.getDefault().register(university.getId());
+            final int iD = Integer.parseInt(university.getId()) - 1;
+            // String aidi = String.valueOf(university.equals(getId()));
+            //Log.d(TAG,aidi);
+            serviceFactory = new ServiceFactory(ApiUrl.BASE_URL);
+            FoodApi service = serviceFactory.createService(FoodApi.class);
+            Call<FoodApi.Food> call = service.callFood();
+            call.enqueue(new Callback<FoodApi.Food>() {
+                @Override
+                public void onResponse(Call<FoodApi.Food> call, Response<FoodApi.Food> response) {
+                    Log.d(TAG, "onResponse");
+
+                    List<FoodApi.FoodList> list = response.body().getFoodList();
+                    //int iD = (int)university.getId();
+
+                    Log.d(TAG,list.size() +"");
+                    //for (int i = 1; i <  1; i++){
+                    Food food = new Food();
+                    food.setId(list.get(iD).getId());
+                    List<FoodApi.Foody> foodyLists = list.get(iD).getFoody();
+                    Log.d(TAG,foodyLists + "");
+
+                    RealmList<FoodRealmObject> foodList = new RealmList<>();
+                    for (int j = 0; j < foodyLists.size(); j ++){
+                        FoodRealmObject foodRealmObject = new FoodRealmObject(foodyLists.get(j).getName(),foodyLists.get(j).getAddress(),
+                                foodyLists.get(j).getImage(),foodyLists.get(j).getTime(),foodyLists.get(j).getPrice());
+                        foodList.add(foodRealmObject);
+                        Log.d(TAG,foodyLists.get(j).getName());
+                    }
+                    food.setFoody(foodList);
+
+
+                    RealmHandler.getInstance().addFoodToRealm(food);
+                    //}
+                    EventBus.getDefault().post(new EventDataReady());
+                    Utils.setLoadData(getActivity(), Constant.keyLoadedFood, true);
+                    progressBar.setVisibility(View.INVISIBLE);
+
+                }
+
+                @Override
+                public void onFailure(Call<FoodApi.Food> call, Throwable t) {
+                    Log.d("No No ", t.toString());
+                }
+            });
+        } else {
+            EventBus.getDefault().post(new EventDataReady());
+        }
+    }
+
 
     private void setupUIFood(View view) {
         layoutManager1 = new GridLayoutManager(
                 view.getContext(), 2, GridLayoutManager.VERTICAL, false);
-        rv_uni_food.setLayoutManager(layoutManager);
+        rv_uni_food1.setLayoutManager(layoutManager1);
 
-        loadFood();
+        loadData();
     }
 
     @Subscribe(sticky = true)
     public void receiveInfo(EventUniversity event){
 
         this.university = event.getUniversity();
-        foodForUniAdapter = new FoodForUniAdapter();
-        foodForUniAdapter.setOnItemClickListener(this);
-        rv_uni_food.setAdapter(foodForUniAdapter);
-        foodForUniAdapter.notifyDataSetChanged();
+//        foodForUniAdapter = new FoodForUniAdapter();
+//        foodForUniAdapter.setOnItemClickListener(this);
+//        rv_uni_food.setAdapter(foodForUniAdapter);
+//        foodForUniAdapter.notifyDataSetChanged();
     }
 
 //    @Subscribe(sticky = true)
@@ -176,12 +247,13 @@ public class InfoUniFragment extends Fragment implements FragmentWithSearch, Vie
 //
 //    }
 
-    private void setupUI() {
+    private void setupUI(View view) {
         tv_name.setText(university.getName());
         //tv_abbreviation.setText(university.getAbbreviation());
         tv_address.setText(university.getAddress());
         //tv_bus.setText(bus.getNumber());
         Glide.with(this).load(Uri.parse("file:///android_asset/logo/" + university.getLogo())).centerCrop().into(iv_university);
+        setupUIFood(view);
 
     }
 
